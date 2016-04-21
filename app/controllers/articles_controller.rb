0da802653a18c1fd6e0748528d216fabf4c08bec
Terry <lcharles@fohmat.com>
2016-04-21 16:40:23 +0000

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ end
   def create
 
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       #do something
       flash[:success] = "Article was a success"
